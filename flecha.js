0b7731af9fea492f1fa273275d55b8338b7f1018
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-11-03 21:39:43 +0100

    funciones de flecha

---
+++ flecha.js
@@ -0,0 +1,14 @@
+// Es la forma tradicional de escribir una función, se emplea hoy en día sólo
+// para determinados usos
+// function sumar( a, b ) {
+//     return a + b;
+// }
+
+// const sumar = (a, b) => {
+//     return a + b;
+// }
+
+// La forma simplificada se emplea cuando sea una función de una sola línea
+const sumar = (a, b) => a+ b;
+
+console.log( sumar(5, 6) );
