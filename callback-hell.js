0ca0b20fb298159f076872573394e841694f2b26
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-11-04 13:49:42 +0100

    callback hell

--- callback-hell.js
+++ callback-hell.js
@@ -13,14 +13,14 @@ const empleados = [
     }
 ];
 
-const salario = [
+const salarios = [
     {
         id: 1,
         salario: 1000
     },
     {
         id: 2,
-        nombre: 1500
+        salario: 1500
     }
 ];
 
@@ -29,18 +29,64 @@ const getEmpleado = ( id, callback ) => {
     const empleado = empleados.find( (e) => e.id === id);
 
     if ( empleado ) {
+        // el null se emplea para gestionar el error en caso de que no se cumpla 
+        // la condición 
         callback(null, empleado);
     } else {
         callback(`Empleado con id ${id} no existe`);
     }
 }
 
-getEmpleado(3, (err, empleado) => {
+// Tarea
+const getSalario = (id, callback) => {
+                                                // ? es el null check operator
+                                                // en caso de que sea null no nos sale error
+                                                // y en caso contrario nos devuelve el valor
+                                                // del key salario
+    const salario = salarios.find((s) => s.id === id)?.salario;
+    if (salario) {
+        callback(null, salario);
+    } else {
+        callback(`Salario con id ${id} no existe`);
+    }
+}
+
+const id = 1;
+
+// Ejemplo de la tarea
+
+// getSalario(id, (err, salario) => {
+//     if(err) {
+//         console.log('ERROR!');
+//         return console.log(err);
+//     }
+
+//     console.log('Salario existe!');
+//     console.log(salario);
+// })
+
+getEmpleado(id, (err, empleado) => {
     if(err) {
+        // sin el null siempre recibiríamos el string 'ERROR!'
         console.log('ERROR!');
         return console.log(err);
     }
 
-    console.log('Empleado existe!');
-    console.log('Empleado');
+    // console.log('Empleado existe!');
+    // console.log(empleado);
+
+    // el callback hell nace cuando debemos hacer una concatenación de callbacks
+    // en este ejemplo, queremos recibir el empleado y el salario de un id, para
+    // ello debemos llamar la función getSalario dentro de getEmpleado
+    // Ahora, si tuviésemos que hacerlo 5, 10 veces se acumularía mucho código que
+    // es muy propenso a errores, por lo que hoy en día ya no se emplea más
+    getSalario(id, (err, salario) => {
+        if(err) {
+            console.log('ERROR!');
+            return console.log(err);
+        }
+
+        console.log(`El empleado ${empleado.nombre} tiene un salario de ${salario}`);
+    })
 })
+
